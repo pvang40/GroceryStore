3c70639be060d4cbeb91d97a8093640a9494e0ce
pvang40 <pvang40@mail.csuchico.edu>
2018-02-21 17:16:33 -0800

    Add do while statement

--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
+//Author: Pachia Vang  
 #include<iostream>
 #include<string>
 using namespace std;
 
+const int LIST_SIZE = 5;
+ 
 int main()
 {
-  string list[5]; //array of 5 strings
+  string groceryList[LIST_SIZE]; //array of 5 strings
   int numItems = 0;
   char input;
 
+do 
+{
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+} while ( input != 'Q' && input != 'q');
   return 0;
 }
