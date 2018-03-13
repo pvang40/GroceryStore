fa87842f2be1f6561420c1a0b16259bf38ea5fcd
pvang40 <pvang40@mail.csuchico.edu>
2018-03-12 17:32:54 -0700

    Added a vector to make the list inputs infinite

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 //Author: Pachia Vang  
 #include<iostream>
 #include<string>
+#include<vector>
+
 using namespace std;
  
 int main()
 {
-  string list[5]; //array of 5 strings
+  vector <string> list; 
   int numItems = 0;
   char input;
   string item;
 
-do 
+do
 {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
@@ -18,33 +20,35 @@ do
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin >> input; 
-
-  if (( input == 'A' || input == 'a' ) && numItems < 5 ) 
+ 
+ 
+  if ( input == 'A' || input == 'a' ) 
   { cout<< "What is the item?\n";  
     cin >> item  ; 
-    list[numItems] = item;
-    numItems ++;
-  }
-
- else if ( numItems > 4 && input != 'q' && input != 'Q' ) 
-  {
-  cout<< "What is the item?";
-  cin>> item; 
-  cout<< "\nYou'll need a bigger list!\n";
- 
+    list.push_back(item);
+   
   } 
 
 
  }while ( input != 'Q' && input != 'q');
  
- int listNumber = 1 ;
+ if (list.size() == 0) 
+ {
+    cout<<"No Items to buy!\n"; 
+ }
+
+ 
+ else if ( list.size() > 0 )
+ { int listNumber = 1; 
+ 
  cout<< "==ITEMS TO BUY==\n";
 
- for ( numItems = 0 ; numItems < 5 ; numItems++ )
+ list.size(); 
+ for ( numItems = 0 ; numItems < list.size() ; numItems++ )
   {
    cout<<listNumber<< " " << list[numItems]<<endl; 
    listNumber++;
   } 
-
+}
   return 0;
 }
